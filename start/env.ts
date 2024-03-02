036416db8a4e644b503077b5972adcef652c9cc5
fralps <francois@kinoba.fr>
2024-03-02 18:46:01 +0100

    fix: deployment

--- start/env.ts
+++ start/env.ts
@@ -13,9 +13,9 @@ import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
+  PORT: Env.schema.number.optional(),
   APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
+  HOST: Env.schema.string.optional({ format: 'host' }),
   LOG_LEVEL: Env.schema.string(),
 
   /*
