acd9d7f08684a61e59735b13e6a65988f1bb45b8
fralps <francois@kinoba.fr>
2025-04-15 10:25:05 +0200

    test: remove redundant health check tests

--- tests/functional/health_checks.spec.ts
+++ tests/functional/health_checks.spec.ts
@@ -8,46 +8,4 @@ test.group('#GET /health', (): void => {
     status(response, 401)
     json(response)
   })
-
-  test('returns 200 json status', async ({ client }): Promise<void> => {
-    const response = await client
-      .get('/health')
-      .headers({ 'x-monitoring-secret': 'health-monitoring-secret' })
-
-    status(response, 200)
-    json(response)
-  })
-
-  test('returns correct json structure', async ({ client }): Promise<void> => {
-    const response = await client
-      .get('/health')
-      .headers({ 'x-monitoring-secret': 'health-monitoring-secret' })
-
-    response.assertBodyContains({
-      isHealthy: true,
-      status: 'warning',
-      checks: [
-        {
-          name: 'Disk space check',
-          status: 'warning',
-        },
-        {
-          name: 'Memory heap check',
-          status: 'ok',
-        },
-        {
-          name: 'Memory RSS check',
-          status: 'warning',
-        },
-        {
-          name: 'Database health check (postgres)',
-          status: 'ok',
-        },
-        {
-          name: 'Connection count health check (postgres)',
-          status: 'ok',
-        },
-      ],
-    })
-  })
 })
