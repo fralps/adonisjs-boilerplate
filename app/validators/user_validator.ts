d7733d9ac8da033c4216d996911bcbf31a676738
fralps <francois@kinoba.fr>
2026-01-20 14:10:39 +0100

    fix: @vinejs/vine vine.compile deprecation warning

--- app/validators/user_validator.ts
+++ app/validators/user_validator.ts
@@ -4,14 +4,12 @@ import { uniqueRule } from '#validators/rules/unique'
 /**
  * Validates the user's creation action
  */
-export const createUserValidator = vine.compile(
-  vine.object({
-    email: vine
-      .string()
-      .email()
-      .use(uniqueRule({ table: 'users', column: 'email' })),
-    password: vine.string().minLength(8).confirmed(),
-    firstName: vine.string(),
-    lastName: vine.string(),
-  })
-)
+export const createUserValidator = vine.create({
+  email: vine
+    .string()
+    .email()
+    .use(uniqueRule({ table: 'users', column: 'email' })),
+  password: vine.string().minLength(8).confirmed(),
+  firstName: vine.string(),
+  lastName: vine.string(),
+})
